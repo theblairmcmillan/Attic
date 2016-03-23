12475f7478f8abf22eb13503713e193d91f71610
BlairMcMillan <theblairmcmillan@gmail.com>
2016-03-23 11:24:57 -0500

    added login  modal components

--- app/login/loginController.js
+++ app/login/loginController.js
@@ -1,11 +1,32 @@
 "use strict";
 app.controller("loginController", ["$scope", "$location", function($scope, $location) {
 
-	// click function on submit button
-	$scope.getUserName = function(text) {
-		console.log("userName", text);
-		$location.url('/all-albums');
-	};
+	// // click function on submit button
+	// $scope.getUserName = function(text) {
+	// 	console.log("userName", text);
+	// 	$location.url('/all-albums');
+	// };
+
+	var ref = new Firebase("https://atticapp.firebaseio.com/");
+	ref = $firebaseAuth(ref);
+
+
+// SIGN UP NEW USER //
+	$scope.userSignUp = function() {
+		console.log("user clicked Sign up Button");
+		var userObj = {
+				email: $scope.userEmail,
+				password: $scope.userPassword
+				
+		};
+		console.log("New User Object",userObj);
+	}
+
+
+
+
+
+
 
 
 

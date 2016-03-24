d389a5511971bd398925c6d11cadd198c687e6a6
BlairMcMillan <theblairmcmillan@gmail.com>
2016-03-23 21:55:34 -0500

    completed authentication and logout

---
+++ app/login/authFactory.js
@@ -0,0 +1,39 @@
+"use strict";
+
+app.factory("authFactory", function () {
+
+  let ref = new Firebase("https://atticapp.firebaseio.com/");
+
+  return {
+    /*
+      Determine if the client is authenticated
+     */
+    isAuthenticated () {
+      let authData = ref.getAuth();
+
+      if (authData) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    /*
+      Authenticate the client via Firebase
+     */
+    authenticate (credentials) {
+      return new Promise((resolve, reject) => {
+        ref.authWithPassword({
+          "email": credentials.email,
+          "password": credentials.password,
+        }, (error, authData) => {
+          if (error) {
+            reject(error);
+          } else {
+            console.log("Successfully completed authorization.");
+            resolve(authData);
+          }
+        });
+      });
+    }
+  };
+});
